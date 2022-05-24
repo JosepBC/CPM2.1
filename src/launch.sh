c44a313469a0996bbc36000f44a9e1b1c2fc4c46
Josep Bello <bellocontacto@gmail.com>
2022-05-24 19:50:24 +0200

    Added -p to time

--- src/launch.sh
+++ src/launch.sh
@@ -9,7 +9,7 @@ for proc in ${procs_per_node[@]}; do
     for node in ${n_nodes[@]}; do      
         let total_procs=${proc}*${node}
         echo ${proc} processos per node, ${node} nodes, total proc ${total_procs}--------------
-        salloc -p pop -N 8 -n 32 time mpirun -n ${total_procs} -npernode ${proc} ./build/par.out 2>> out/${proc}_${node}_${total_procs}.out
+        salloc -p pop -N 8 -n 32 time -p mpirun -n ${total_procs} -npernode ${proc} ./build/par.out 2>> out/${proc}_${node}_${total_procs}.out
     done
 done
 
@@ -19,7 +19,7 @@ for proc in ${procs_per_node[@]}; do
     for node in ${n_nodes[@]}; do      
         let total_procs=${proc}*${node}
         echo ${proc} processos per node, ${node} nodes, total proc ${total_procs}--------------
-        salloc -p pop -N 8 -n 32 time mpirun -n ${total_procs} -npernode ${proc} ./build/par.out 2>> out/${proc}_${node}_${total_procs}.out
+        salloc -p pop -N 8 -n 32 time -p mpirun -n ${total_procs} -npernode ${proc} ./build/par.out 2>> out/${proc}_${node}_${total_procs}.out
     done
 done
 
@@ -29,10 +29,10 @@ for proc in ${procs_per_node[@]}; do
     for node in ${n_nodes[@]}; do      
         let total_procs=${proc}*${node}
         echo ${proc} processos per node, ${node} nodes, total proc ${total_procs}--------------
-        salloc -p pop -N 8 -n 32 time mpirun -n ${total_procs} -npernode ${proc} ./build/par.out 2>> out/${proc}_${node}_${total_procs}.out
+        salloc -p pop -N 8 -n 32 time -p mpirun -n ${total_procs} -npernode ${proc} ./build/par.out 2>> out/${proc}_${node}_${total_procs}.out
     done
 done
 
-salloc -p pop -N 8 -n 32 time mpirun -n 64 --mca mpi_yield_when_idle true --oversubscribe ./build/par.out 2>> out/4_16_64.out
-salloc -p pop -N 8 -n 32 time mpirun -n 128 --mca mpi_yield_when_idle true --oversubscribe ./build/par.out 2>> out/4_32_128.out
+salloc -p pop -N 8 -n 32 time -p mpirun -n 64 --mca mpi_yield_when_idle true --oversubscribe ./build/par.out 2>> out/4_16_64.out
+salloc -p pop -N 8 -n 32 time -p mpirun -n 128 --mca mpi_yield_when_idle true --oversubscribe ./build/par.out 2>> out/4_32_128.out
 exit 0
